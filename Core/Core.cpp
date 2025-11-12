0daf80babfa58ec48ab0e47166026c20e60c3cb1
ales2026 <ale.urankar@gmail.com>
2025-11-12 21:26:58 +0100

    adding Core library and linking it to ConsoleSandbox

---
+++ Core/Core.cpp
@@ -0,0 +1,11 @@
+// Core.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+int Test()
+{
+	return 100;
+}
