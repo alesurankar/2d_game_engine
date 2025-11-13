e23fd11719991566286900a43c1640bf8a06a0a6
ales2026 <ale.urankar@gmail.com>
2025-11-13 06:38:10 +0100

    adding propertyes

--- Core/Core.cpp
+++ Core/Core.cpp
@@ -1,10 +1,7 @@
 // Core.cpp : Defines the functions for the static library.
 //
 
-#include "pch.h"
-#include "framework.h"
 
-// TODO: This is an example of a library function
 int Test()
 {
 	return 100;
